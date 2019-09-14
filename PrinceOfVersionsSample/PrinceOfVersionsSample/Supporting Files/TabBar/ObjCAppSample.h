e9d44b6c6b944cd452e9f8d571efbf844561cd86
Jasmin Abou Aldan <jasmin.aboualdan@infinum.hr>
2019-09-14 18:36:05 +0200

    Add AppDelegate, Constants, Main.storyboard and base tab bar classes for the sample app

---
+++ PrinceOfVersionsSample/PrinceOfVersionsSample/Supporting Files/TabBar/ObjCAppSample.h
@@ -0,0 +1,17 @@
+//
+//  ObjCAppSample.h
+//  PrinceOfVersionsSample
+//
+//  Created by Jasmin Abou Aldan on 13/09/2019.
+//  Copyright © 2019 infinum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ObjCAppSample : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PrinceOfVersionsSample/PrinceOfVersionsSample/Supporting Files/TabBar/ObjCAppSample.m
@@ -0,0 +1,13 @@
+//
+//  ObjCAppSample.m
+//  PrinceOfVersionsSample
+//
+//  Created by Jasmin Abou Aldan on 13/09/2019.
+//  Copyright © 2019 infinum. All rights reserved.
+//
+
+#import "ObjCAppSample.h"
+
+@implementation ObjCAppSample
+
+@end
