0e9a1c1473e7b2604711446f6e0b2f4fb2e2774e
Jasmin Abou Aldan <jasmin.aboualdan@infinum.hr>
2019-09-14 18:36:37 +0200

    Add ObjC Version of the sample app

---
+++ PrinceOfVersionsSample/PrinceOfVersionsSample/ObjectiveC Implementation/AutomaticCheckViewController/ObjCAutomaticCheckViewController.h
@@ -0,0 +1,17 @@
+//
+//  ObjCAutomaticCheckViewController.h
+//  PrinceOfVersionsSample
+//
+//  Created by Jasmin Abou Aldan on 13/09/2019.
+//  Copyright © 2019 infinum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ObjCAutomaticCheckViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
