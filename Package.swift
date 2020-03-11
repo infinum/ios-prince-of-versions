9e1e3f7893433ec9c58549222d57d3256133f205
Jasmin Abou Aldan <jasmin.aboualdan@infinum.com>
2020-03-11 11:59:38 +0100

    Fix missing test resources

--- Package.swift
+++ Package.swift
@@ -11,9 +11,7 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "PrinceOfVersions",
-            targets: ["PrinceOfVersions"]),
+        .library(name: "PrinceOfVersions", targets: ["PrinceOfVersions"])
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
