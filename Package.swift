9e2bec9c50cd6549a67fd301f800126879f0b062
Jasmin Abou Aldan <jasmin.aboualdan@infinum.com>
2020-03-12 15:24:32 +0100

    Revert "Fix modile.map include folder"
    This reverts commit 058138e89f5453731f87cc599767e7ee04d5f5a7.

--- Package.swift
+++ Package.swift
@@ -16,8 +16,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "PrinceOfVersionsObjc", dependencies: [], path: "PrinceOfVersionsObjC"),
-        .target(name: "PrinceOfVersions", dependencies: ["PrinceOfVersionsObjc"], path: "PrinceOfVersions"),
+        .target(name: "PrinceOfVersions", dependencies: [], path: "PrinceOfVersions"),
         .testTarget(name: "PrinceOfVersionsTests", dependencies: ["PrinceOfVersions"], path: "PrinceOfVersionsTests")
     ]
 )
