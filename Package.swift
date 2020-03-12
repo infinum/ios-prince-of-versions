a6ed7061c9f0f6bbbcbfff6d096e63bbda81a2db
Jasmin Abou Aldan <jasmin.aboualdan@infinum.com>
2020-03-12 15:17:49 +0100

    Fix modile.map include folder

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "PrinceOfVersions", dependencies: [], path: "PrinceOfVersions"),
+        .target(name: "PrinceOfVersionsObjc", dependencies: [], path: "PrinceOfVersionsObjC"),
+        .target(name: "PrinceOfVersions", dependencies: ["PrinceOfVersionsObjc"], path: "PrinceOfVersions"),
         .testTarget(name: "PrinceOfVersionsTests", dependencies: ["PrinceOfVersions"], path: "PrinceOfVersionsTests")
     ]
 )
