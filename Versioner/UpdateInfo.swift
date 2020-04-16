79b55431deaea85f19c4232620d428d523238610
ivanamrsic <ivana.mrsic@fer.hr>
2020-04-16 09:42:20 +0200

    change manual decoding to codable on UpdateInfo and Version structs

--- Versioner/UpdateInfo.swift
+++ Versioner/UpdateInfo.swift
@@ -34,29 +34,74 @@ extension UpdateInfoValues {
 
 // MARK: - Internal configuration data -
 
-public struct UpdateInfo {
+public struct UpdateInfo: Decodable {
 
-    // MARK: - Private configuration -
-    
-    /// Data parsed from the configuration file
-    private struct ConfigurationData {
-        var minimumRequiredVersion: Version?
-        var minimumSdkForMinimumRequiredVersion: Version?
-        var latestVersion: Version?
-        var minimumSdkForLatestVersion: Version?
-        var installedVersion: Version?
-        var sdkVersion: Version?
-        var metadata: [String: Any]?
+    private let ios: ConfigurationData?
+    private let macos: ConfigurationData?
+    private let meta: [String: Id<Any>]?
+
+    enum CodingKeys: String, CodingKey {
+        case ios
+        case macos
+        case meta
     }
 
-    private var configurationData = ConfigurationData()
+    private struct ConfigurationData: Codable {
+        let minimumVersion: Version?
+        let minimumSdkForMinimumRequiredVersion: Version?
+        let latestVersion: LatestVersion?
+
+        enum CodingKeys: String, CodingKey {
+            case minimumVersion = "minimum_version"
+            case minimumSdkForMinimumRequiredVersion = "minimum_version_min_sdk"
+            case latestVersion = "latest_version"
+        }
+    }
+
+    private struct LatestVersion: Codable {
+        let version: Version?
+        let notificationType: UpdateInfo.NotificationType?
+        let minimumSdk: Version?
+
+        enum CodingKeys: String, CodingKey {
+            case version = "version"
+            case notificationType = "notification_type"
+            case minimumSdk = "min_sdk"
+        }
+    }
+
+    // MARK: - Private properties -
+
+    private var configurationForOS: ConfigurationData? {
+        #if os(iOS)
+        return ios
+        #elseif os(macOS)
+        return macos
+        #endif
+    }
+
+    private var currentSdkVersion: Version? {
+        #if os(iOS)
+        return try Version(string: UIDevice.current.systemVersion)
+        #elseif os(macOS)
+        return Version(macVersion: ProcessInfo.processInfo.operatingSystemVersion)
+        #endif
+    }
+
+    private let bundle = Bundle.main
 
     // MARK: - Public notification type
-    
-    public enum NotificationType : String {
+
+    public enum NotificationType: String, Codable {
         case always = "ALWAYS"
         case once = "ONCE"
+
+        enum CodingKeys: CodingKey {
+            case always
+            case once
+        }
     }
+
     // MARK: - Public properties
 
     /**
@@ -68,84 +113,26 @@ public struct UpdateInfo {
 
      Default value is `.once`
      */
-    public private(set) var notificationType: UpdateInfo.NotificationType = .once
-
-    // MARK: - Init -
-    init(data: Data?, bundle: Bundle = Bundle.main) throws {
-        guard let data = data else {
-            throw PrinceOfVersionsError.invalidJsonData
-        }
-        let json = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary
-
-        // JSON data
-        guard let value = json as? [String: AnyObject] else {
-            throw PrinceOfVersionsError.invalidJsonData
-        }
-
-        #if os(iOS)
-        guard let os = value["ios"] as? [String: AnyObject] else {
-            throw PrinceOfVersionsError.invalidJsonData
-        }
-
-        #elseif os(macOS)
-        guard let os = value["macos"] as? [String: AnyObject] else {
-            throw PrinceOfVersionsError.invalidJsonData
-        }
-        #else
-        throw PrinceOfVersionsError.invalidJsonData
-        #endif
-
-        // Minimum version
-        if let minimumVersion = os["minimum_version"] as? String {
-            configurationData.minimumRequiredVersion = try? Version(string: minimumVersion)
-        }
-        
-        // Minimum sdk for minimum version
-        if let minimumSdkForMinimumVersionString = os["minimum_version_min_sdk"] as? String {
-            configurationData.minimumSdkForMinimumRequiredVersion = try? Version(string: minimumSdkForMinimumVersionString)
-        }
-
-        // Latest version and notification type
-        guard let latestVersionInfo = os["latest_version"] as? [String: AnyObject] else {
-            throw PrinceOfVersionsError.invalidLatestVersion
-        }
-
-        let notificationTypeString = (latestVersionInfo["notification_type"] as? String) ?? ""
-
-        if let notificationTypeFromString = NotificationType(rawValue: notificationTypeString) {
-            notificationType = notificationTypeFromString
-        }
-
-        if let versionString = latestVersionInfo["version"] as? String {
-            configurationData.latestVersion = try Version(string: versionString)
-        } else {
-            throw PrinceOfVersionsError.invalidLatestVersion
-        }
-        
-        if let minimumSdkForLatestVersionString = latestVersionInfo["min_sdk"] as? String {
-            configurationData.minimumSdkForLatestVersion = try? Version(string: minimumSdkForLatestVersionString)
-        }
-
-        // Installed version
-        guard let currentVersionString = bundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else {
-            throw PrinceOfVersionsError.invalidCurrentVersion
-        }
-        guard let currentBuildNumberString = bundle.object(forInfoDictionaryKey: "CFBundleVersion") as? String else {
-            throw PrinceOfVersionsError.invalidCurrentVersion   
-        }
-
-        configurationData.installedVersion = try Version(string: currentVersionString + "-" + currentBuildNumberString)
-
-         #if os(iOS)
-        configurationData.sdkVersion = try Version(string: UIDevice.current.systemVersion)
-        #elseif os(macOS)
-        configurationData.sdkVersion = Version(macVersion: ProcessInfo.processInfo.operatingSystemVersion)
-        #endif
-
-        // Metadata
-        configurationData.metadata = value["meta"] as? [String: Any]
+    public var notificationType: UpdateInfo.NotificationType {
+        return configurationForOS?.latestVersion?.notificationType ?? .once
     }
 
+    private var currentInstalledVersion: Version? {
+
+        guard
+            let currentVersionString = bundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String,
+            let currentBuildNumberString = bundle.object(forInfoDictionaryKey: "CFBundleVersion") as? String
+        else {
+//            throw PrinceOfVersionsError.invalidCurrentVersion
+            return nil
+        }
+
+        do {
+            return try Version(string: currentVersionString + "-" + currentBuildNumberString)
+        } catch _ {
+            return nil
+        }
+    }
 }
 
 // MARK: - UpdateInfoValues -
@@ -156,21 +143,22 @@ extension UpdateInfo: UpdateInfoValues {
      Returns minimum required version of the app.
      */
     public var minimumRequiredVersion: Version? {
-        return configurationData.minimumRequiredVersion
+        return configurationForOS?.minimumVersion
     }
 
     /**
      Returns minimum sdk for minimum required version of the app.
      */
     public var minimumSdkForMinimumRequiredVersion: Version? {
-        return configurationData.minimumSdkForMinimumRequiredVersion
+        return configurationForOS?.minimumSdkForMinimumRequiredVersion
     }
 
     /**
      Returns latest available version of the app.
      */
     public var latestVersion: Version {
-        guard let version = configurationData.latestVersion else {
+//        throw PrinceOfVersionsError.invalidLatestVersion
+        guard let version = configurationForOS?.latestVersion?.version else {
             preconditionFailure("Missing requred latest version data")
         }
         return version
@@ -180,14 +168,14 @@ extension UpdateInfo: UpdateInfoValues {
      Returns sdk for latest available version of the app.
      */
     public var minimumSdkForLatestVersion: Version? {
-        return configurationData.minimumSdkForLatestVersion
+        return configurationForOS?.latestVersion?.minimumSdk
     }
 
     /**
      Returns installed version of the app.
      */
     public var installedVersion: Version {
-        guard let version = configurationData.installedVersion else {
+        guard let version = currentInstalledVersion else {
             preconditionFailure("Unable to get installed version data")
         }
         return version
@@ -197,7 +185,7 @@ extension UpdateInfo: UpdateInfoValues {
      Returns sdk version of device.
      */
     public var sdkVersion: Version {
-        guard let version = configurationData.sdkVersion else {
+        guard let version = currentSdkVersion else {
             preconditionFailure("Unable to get sdk version data")
         }
         return version
@@ -224,6 +212,6 @@ extension UpdateInfo: UpdateInfoValues {
      Key-value pairs under "meta" key are optional metadata of which any amount can be sent accompanying the required fields.
      */
     public var metadata: [String : Any]? {
-        return configurationData.metadata
+        return meta?.mapValues { $0.raw }
     }
 }
