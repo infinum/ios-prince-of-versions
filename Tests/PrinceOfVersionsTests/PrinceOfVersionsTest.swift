7edb696e132a12950b5d55e2d803d3319c196570
ivanamrsic <ivana.mrsic@fer.hr>
2020-05-29 11:27:21 +0200

    work on tests, change code organization

--- Tests/PrinceOfVersionsTests/PrinceOfVersionsTest.swift
+++ Tests/PrinceOfVersionsTests/PrinceOfVersionsTest.swift
@@ -11,63 +11,28 @@ import XCTest
 
 class PrinceOfVersionsTest: XCTestCase {
 
-    static let testURL = URL(string: "https://pastebin.com/raw/ZAfWNZCi")!
+    static let testURL = URL(string: "https://pastebin.com/raw/0MfYmWGu")!
 
     func testLoadConfigurationBaseOnMain() {
         runAsyncTest { finished in
-            PrinceOfVersions().loadConfiguration(
+            PrinceOfVersions.checkForUpdates(
                 from: PrinceOfVersionsTest.testURL,
-                completion: { _ in
-                    XCTAssertTrue(Thread.isMainThread)
-                    finished()
+                completion: {  _ in
+                   XCTAssertTrue(Thread.isMainThread)
+                   finished()
             })
         }
     }
 
     func testLoadConfigurationBaseOnBackground() {
         runAsyncTest { finished in
-            PrinceOfVersions().loadConfiguration(
+            PrinceOfVersions.checkForUpdates(
                 from: PrinceOfVersionsTest.testURL,
                 callbackQueue: .global(qos: .background),
                 completion: { _ in
                     XCTAssertFalse(Thread.isMainThread)
                     finished()
             })
-        }
-    }
-
-    func testCheckForUpdatesBaseOnMain() {
-        runAsyncTest { finished in
-            PrinceOfVersions().checkForUpdates(
-                from: PrinceOfVersionsTest.testURL,
-                newVersion: { _, _, _  in
-                    XCTAssertTrue(Thread.isMainThread)
-                    finished()
-                }, noNewVersion: { _, _  in
-                    XCTAssertTrue(Thread.isMainThread)
-                    finished()
-                }, error: { _ in
-                    XCTAssertTrue(Thread.isMainThread)
-                    finished()
-                })
-        }
-    }
-
-    func testCheckForUpdatesBaseOnBackground() {
-        runAsyncTest { finished in
-            PrinceOfVersions().checkForUpdates(
-                from: PrinceOfVersionsTest.testURL,
-                callbackQueue: .global(qos: .background),
-                newVersion: { _, _, _ in
-                    XCTAssertFalse(Thread.isMainThread)
-                    finished()
-                }, noNewVersion: { _, _ in
-                    XCTAssertFalse(Thread.isMainThread)
-                    finished()
-                }, error: { _ in
-                    XCTAssertFalse(Thread.isMainThread)
-                    finished()
-                })
         }
     }
 
@@ -77,20 +42,20 @@ class PrinceOfVersionsTest: XCTestCase {
         let jsonPath = bundle.path(forResource: "app_store_version_example", ofType: "json")!
 
         let installedVersion = try! Version(string: "1.0.0-1")
-        let latestVersion = try! Version(string: "0.1.0")
+        let lastVersionAvailable = try! Version(string: "0.1.0")
         let minimumSdkForLatestVersion = try! Version(string: "9.0")
 
         runAsyncTest { finished in
-            PrinceOfVersions().internalyGetDataFromAppStore(URL(fileURLWithPath: jsonPath), trackPhaseRelease: false, bundle: bundle, testMode: true, completion: { result in
+            PrinceOfVersions.internalyGetDataFromAppStore(URL(fileURLWithPath: jsonPath), trackPhaseRelease: false, bundle: bundle, testMode: true, completion: { result in
                 switch result {
                 case .success(let info):
                     XCTAssert(info.installedVersion == installedVersion)
-                    XCTAssert(info.latestVersion == latestVersion)
-                    if let minSdkForLatestVersion = info.minimumSdkForLatestVersion {
-                        XCTAssert(minSdkForLatestVersion == minimumSdkForLatestVersion)
-                    } else {
-                        XCTFail("min sdk should not be nil")
-                    }
+                    XCTAssert(info.lastVersionAvailable == lastVersionAvailable)
+//                    if let minSdkForLatestVersion = info.versionInfo.sdkVersion {
+//                        XCTAssert(minSdkForLatestVersion == minimumSdkForLatestVersion)
+//                    } else {
+//                        XCTFail("min sdk should not be nil")
+//                    }
                     XCTAssert(!info.phaseReleaseInProgress)
                     finished()
                 case .failure:
@@ -105,7 +70,7 @@ class PrinceOfVersionsTest: XCTestCase {
         let bundle = Bundle(for: type(of: self))
         let jsonPath = bundle.path(forResource: "app_store_version_example", ofType: "json")!
         runAsyncTest { finished in
-            PrinceOfVersions().internalyGetDataFromAppStore(URL(fileURLWithPath: jsonPath), trackPhaseRelease: true, bundle: bundle, testMode: true, completion: { result in
+            PrinceOfVersions.internalyGetDataFromAppStore(URL(fileURLWithPath: jsonPath), trackPhaseRelease: true, bundle: bundle, testMode: true, completion: { result in
                 switch result {
                 case .success(let info):
                     XCTAssert(!info.phaseReleaseInProgress)
